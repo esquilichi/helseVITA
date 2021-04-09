0994fca25d7a3c478d897cfdee4c8d7cea41f532
Skayara <79339304+Skayara@users.noreply.github.com>
2021-04-09 11:33:04 +0200

    Creado el @Service de Patient

---
+++ src/main/java/spring/io/PatientService.java
@@ -0,0 +1,117 @@
+package spring.io;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class PatientService {
+
+    private Map <Patient, HealthPersonnel> map = new ConcurrentHashMap< Patient, HealthPersonnel>();
+    //private Centro centro;
+    //private Map<Long, User> map = new ConcurrentHashMap<Long, User>();
+    private Long lastId = (long) -1;
+
+    public User addPatient(Patient patient, HealthPersonnel healthPersonnel) {
+        lastId++;
+        patient.setId(lastId);
+        this.map.put(patient, healthPersonnel);
+        return patient;
+    }
+
+    public boolean exists(Long id) {
+        for (Map.Entry <Patient, HealthPersonnel> entry : map.entrySet()){
+            if(entry.getKey().getId()==id)
+            return true;
+        }
+        return false;
+    }
+
+    public void editHealthPersonnel(Long id, HealthPersonnel healthPersonnel, Patient patient) {
+        if (this.exists(id)) {
+            this.map.put(patient, healthPersonnel);
+        }
+    }
+
+    public void deleteUser(Long id) {
+        for (Map.Entry <Patient, HealthPersonnel> entry : map.entrySet()){
+            if(entry.getKey().getId()==id){
+                Patient patient= (Patient) entry.getKey();
+                this.map.remove(patient);
+            }
+        }
+    }
+
+    public Collection<Patient> returnAll() {
+        return this.map.keySet();
+    }
+
+    public Patient search (String username) {
+        
+        for (Map.Entry <Patient, HealthPersonnel> entry : map.entrySet()){
+            if(entry.getKey().getUsername().equals(username)){
+                return entry.getKey();
+            }
+        }
+        return null;
+    }
+
+    public void updateUsername(String username, long id) {
+        
+        for (Map.Entry <Patient, HealthPersonnel> entry : map.entrySet()){
+            if(entry.getKey().getId()==id){
+                entry.getKey().setUsername(username);
+                break;
+            }
+        }
+    }
+
+    public void updatePassword(String password, Long id) {
+        for (Map.Entry <Patient, HealthPersonnel> entry : map.entrySet()){
+            if(entry.getKey().getId()==id){
+                entry.getKey().setPassword(password);
+                break;
+            }
+        }
+    }
+
+    public void updateEmail(String email, Long id) {
+        for (Map.Entry <Patient, HealthPersonnel> entry : map.entrySet()){
+            if(entry.getKey().getId()==id){
+                entry.getKey().setEmail(email);
+                break;
+            }
+        }
+    }
+
+    public void updateDNI(String dni, Long id) {
+        for (Map.Entry <Patient, HealthPersonnel> entry : map.entrySet()){
+            if(entry.getKey().getId()==id){
+                entry.getKey().setdni(dni);
+                break;
+            }
+        }
+    }
+
+    /*public Centro getCentro() {
+        return this.centro;
+    }
+
+    public void setCentro(Centro centro) {
+        this.centro = centro;
+    }*/
+
+    public HealthPersonnel getHealthPersonnel(Patient patient){
+        return this.map.get(patient);
+    }
+
+    public void updateHealthPersonnel(HealthPersonnel healthPersonnel, Patient patient){
+        if(exists(healthPersonnel.getId())){
+            for (Map.Entry <Patient, HealthPersonnel> entry : map.entrySet()){
+                if(entry.getKey().equals(patient)){
+                    this.map.replace(patient, healthPersonnel);
+                    break;
+                }
+            }
+        }
+    }
+}
