b8cd86650ba24b48cc7ef78630fb021192e6df5a
Skayara <79339304+Skayara@users.noreply.github.com>
2021-04-12 10:25:45 +0200

    Update

--- src/main/java/spring/io/HealthPersonnel.java
+++ src/main/java/spring/io/HealthPersonnel.java
@@ -1,12 +1,10 @@
 package spring.io;
 
-import java.util.Map;
-
 public class HealthPersonnel extends User{
 
     public HealthPersonnel() {}
 
-    public HealthPersonnel(String username, String password, String email, String dni, Long id, Map<Integer, String> pacientes , Centro centro) {
+    public HealthPersonnel(String username, String password, String email, String dni, Long id) {
         super(username, password, email, dni, id);
     }
 }
