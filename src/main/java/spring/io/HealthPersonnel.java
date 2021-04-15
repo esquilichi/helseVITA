40ae76fc97c62705afe4b3e4b7658dc6ccf20186
imaneky <55080338+imaneky@users.noreply.github.com>
2021-04-15 22:16:28 +0200

    color boton

--- src/main/java/spring/io/HealthPersonnel.java
+++ src/main/java/spring/io/HealthPersonnel.java
@@ -7,4 +7,6 @@ public class HealthPersonnel extends User{
     public HealthPersonnel(String username, String password, String email, String dni, Long id) {
         super(username, password, email, dni, id);
     }
+
+
 }
