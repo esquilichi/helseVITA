dc7404992c69403af8fba332310240908da191d1
Ismael Esquilichi <67276772+esquilichi@users.noreply.github.com>
2021-04-28 16:37:49 +0200

    PatientRestControl Acabado

--- src/main/java/com/urjc/es/helseVITA/Controllers/PatientRestControl.java
+++ src/main/java/com/urjc/es/helseVITA/Controllers/PatientRestControl.java
@@ -1,12 +1,83 @@
 package com.urjc.es.helseVITA.Controllers;
 
+import com.urjc.es.helseVITA.Services.PatientService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import com.urjc.es.helseVITA.Entities.*;
+import java.util.Collection;
 
 @RestController
 public class PatientRestControl {
 
     @Autowired
+    PatientService patientService;
 
+    @PostMapping("/api/patients")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Patient newPatient(@RequestBody Patient patient){
+        return patientService.addPatient(patient);
+    }
+
+    @PutMapping("/api/patients/{id}")
+    public ResponseEntity<Patient> updatePatient(@PathVariable Integer id, @RequestBody Patient patient){
+        if (patientService.exists(id)){
+            return new ResponseEntity<>(patientService.addPatient(patient), HttpStatus.OK);
+        }else{
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/api/patients/{id}")
+    public ResponseEntity<Patient> deletePatient(@PathVariable Integer id){
+        if (patientService.exists(id)){
+            return  new ResponseEntity<>(HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    //Get one specified user
+    @GetMapping("/api/patients/{id}")
+    public ResponseEntity<Patient> getSinglePatient(@PathVariable Integer id){
+        if (patientService.exists(id)){
+            return new ResponseEntity<>(patientService.returnPatient(id),HttpStatus.OK);
+        }else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+    //Get All Users
+    @GetMapping("/api/patients")
+    @ResponseStatus(HttpStatus.OK)
+    public Collection<Patient> returnAll(){
+        return patientService.returnAllPatients();
+    }
+
+    @PatchMapping("/api/patiends/{id}")
+    public ResponseEntity<Patient> patchPatient(@PathVariable Integer id, @RequestBody Patient patient){
+        if (patientService.exists(id)){
+            //Get actual Patient with that ID
+            Patient temp = patientService.returnPatient(id);
+
+            if (patient.getNombre() != null)
+                temp.setNombre(patient.getNombre());
+            if(patient.getApellido1() != null)
+                temp.setApellido1(patient.getApellido1());
+            if (patient.getApellido2() != null)
+                temp.setApellido2(patient.getApellido2());
+            if (patient.getDni() != null)
+                temp.setDni(patient.getDni());
+            if (patient.getEmail() != null)
+                temp.setEmail(patient.getEmail());
+            if (patient.getPassword() != null)
+                temp.setPassword(patient.getPassword());
+            if (patient.getUsername() != null)
+                temp.setUsername(patient.getUsername());
+            if (patient.getEdad() < 0)
+                temp.setEdad(patient.getEdad());
+
+            return new ResponseEntity<>(patientService.addPatient(temp),HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
 }
--- src/main/java/com/urjc/es/helseVITA/Services/PatientService.java
+++ src/main/java/com/urjc/es/helseVITA/Services/PatientService.java
@@ -1,2 +1,56 @@
-package com.urjc.es.helseVITA.Services;public class PatientService {
+package com.urjc.es.helseVITA.Services;
+
+import com.urjc.es.helseVITA.Entities.Patient;
+import com.urjc.es.helseVITA.Repositories.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PatientService {
+
+    @Autowired
+    PatientRepository patientRepository;
+
+    public Patient addPatient(Patient patient){
+        return patientRepository.save(patient);
+    }
+
+    public boolean exists(Integer id){
+        //Hay que mirar si funciona, deberia devolver Optional<>
+        if (patientRepository.findById(id) != null){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void delete(Integer id){
+        Patient temp;
+        Optional<Patient> tempOptional = patientRepository.findById(id);
+            if (tempOptional.isPresent()) {
+                temp = tempOptional.get();
+                patientRepository.delete(temp);
+            }
+    }
+
+    public Patient returnPatient(Integer id){
+        Patient temp;
+        Optional<Patient> tempOptional = patientRepository.findById(id);
+        if (tempOptional.isPresent()){
+            temp = tempOptional.get();
+            return temp;
+        }
+        return null;
+    }
+
+    public Collection<Patient> returnAllPatients(){
+        List<Patient> list = new ArrayList<>();
+        patientRepository.findAll().forEach(list::add);
+        return list;
+    }
 }
