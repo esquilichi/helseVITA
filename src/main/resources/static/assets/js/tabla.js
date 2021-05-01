78acba88985dd7fc7b16cae6d6baf72bbce34311
Ismael Esquilichi <67276772+esquilichi@users.noreply.github.com>
2021-05-01 19:11:46 +0200

    DINAMISMOS MEJORADOS :)
    Escrito más código Vanilla JS para realizar filtrado dinámico sin pasar por clases de Java, directamente en tabla HTML

--- src/main/resources/static/assets/js/tabla.js
+++ src/main/resources/static/assets/js/tabla.js
@@ -1,4 +1,4 @@
-function myFunction(){
+function edad(){
     let input, filter, table, tr, th, i, txtValue;
     input = document.getElementById("myInput");
     filter = input.value.toUpperCase();
@@ -9,7 +9,60 @@ function myFunction(){
         if (th){
             txtValue = th.textContent || th.innerText;
             if (txtValue.toUpperCase().indexOf(filter) > -1){
-                tr[i].style.display = "";
+                if (tr[i].style.display === ""){
+                    tr[i].style.display = "";
+                }else if (tr[i].style.display === "none"){
+                    tr[i].style.display = "none";
+                }
+
+            }else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
+
+function cambia_id(){
+    let input, filter, table, tr, th, i, txtValue;
+    input = document.getElementById("myInput2");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("myTable");
+    tr  = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++){
+        th = tr[i].getElementsByTagName("th")[1];
+        if (th){
+            txtValue = th.textContent || th.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1){
+                if (tr[i].style.display === ""){
+                    tr[i].style.display = "";
+                }else if (tr[i].style.display === "none"){
+                    tr[i].style.display = "none";
+                }
+
+            }else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
+
+function cambia_apellido1(){
+    let input, filter, table, tr, th, i, txtValue;
+    input = document.getElementById("myInput3");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("myTable");
+    tr  = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++){
+        th = tr[i].getElementsByTagName("th")[2];
+        if (th){
+            txtValue = th.textContent || th.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1){
+                if (tr[i].style.display === ""){
+                    tr[i].style.display = "";
+                }else if (tr[i].style.display === "none"){
+                    tr[i].style.display = "none";
+                }
+
             }else {
                 tr[i].style.display = "none";
             }
