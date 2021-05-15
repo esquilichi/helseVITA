bb7bae73d9dec6451a1fdaed6ce0553200ef257a
Skayara <79339304+Skayara@users.noreply.github.com>
2021-05-16 00:45:51 +0200

    Suceso extraño
    No sé porqué me sale q se ha hecho commit de este archivo en blanco. Lo vuelvo a hacer.

---
+++ src/main/java/com/urjc/es/helseVITA/Repositories/AdminRepository.java
@@ -0,0 +1,9 @@
+package com.urjc.es.helseVITA.Repositories;
+
+import com.urjc.es.helseVITA.Entities.Admin;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdminRepository extends JpaRepository<Admin, Integer>{
+    
+}
