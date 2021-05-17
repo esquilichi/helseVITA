2f78683db934e79ac156fefd6c5839c0f3ee1c14
Denisa Medovarschi <denyhack3r@gmail.com>
2021-05-18 00:39:06 +0200

    pagina mi perfil privada

--- src/main/java/com/urjc/es/helseVITA/Security/SecurityConfig.java
+++ src/main/java/com/urjc/es/helseVITA/Security/SecurityConfig.java
@@ -18,72 +18,74 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
-    
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
 
     @Autowired
     public RepositoryUserDetailsService userDetailsService;
 
 
     public EnumRolUsers rol;
-    
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
     }
-    
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder(12, new SecureRandom());
-	}
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(12, new SecureRandom());
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-    	http
-        
-            .authorizeRequests()
+        http
 
-                .antMatchers("/index", "/login", "/", "/loginError", "/logout", "/exito", "/exito-contacto", 
+                .authorizeRequests()
 
-                    "/contact-us", "/faq", "/myHelsevita", "/search-center", "/work-with-us", "/error", "/insurance" )   //Aquí se ponen las rutas que se permiten a ese rol (Anónimo en este caso)     
+                .antMatchers("/index", "/login", "/", "/loginError", "/logout", "/exito", "/exito-contacto",
+
+                        "/contact-us", "/faq", "/myHelsevita", "/search-center", "/work-with-us", "/error", "/insurance", "/politica")   //Aquí se ponen las rutas que se permiten a ese rol (Anónimo en este caso)
 
                 .permitAll()
 
                 //privadas
 
-                .antMatchers("/appointmentAlreadyExist/**","/appointment", "/appointmentNotFound", "/asignarNuevoPaciente", "/asignarNuevoSanitario", 
-                    "/buscarPaciente", "/buscarSanitario", "/citaAgregada", "/cualDoctor", "/areaAdmin", "/areaPaciente", "/areaSanitario", 
-                    "/crearPaciente", "/crearSanitario", "/indexAuth", "/loginExito", "/mostrarCitas", "/nuevaCita", "/user-not-found",
-                    "/mostrarPacientes", "/mostrarSanitario", "/userAlreadyExists", "/admin/**" , "/mostrar/**", "/asignarNuevoPaciente").hasAnyRole("ADMIN") //Páginas permitidas para Admin
+                .antMatchers("/appointmentAlreadyExist/**", "/appointment", "/appointmentNotFound", "/asignarNuevoPaciente", "/asignarNuevoSanitario",
+                        "/buscarPaciente", "/buscarSanitario", "/citaAgregada", "/cualDoctor", "/areaAdmin", "/areaPaciente", "/areaSanitario",
+                        "/crearPaciente", "/crearSanitario", "/indexAuth", "/loginExito", "/mostrarCitas", "/nuevaCita", "/user-not-found",
+                        "/mostrarPacientes", "/mostrarSanitario", "/userAlreadyExists", "/admin/**", "/mostrar/**", "/asignarNuevoPaciente").hasAnyRole("ADMIN") //Páginas permitidas para Admin
 
-                .antMatchers( "/areaSanitario", "/indexAuth", "/loginExito", "/mostrarPacientes").hasAnyRole("HEALTHPERSONNEL") //Páginas permitidas para Sanitario
-                .antMatchers("/preguntasSinContestar","/preguntasSinContestar/**","/contestarPregunta/**").hasAnyRole("ADMIN","HEALTHPERSONNEL")
-                .antMatchers("/appointmentAlreadyExist/**","/appointment", "/appointmentNotFound", "/citaAgregada", "/cualDoctor",  "/areaPaciente", 
-                    "/indexAuth", "/loginExito", "/mostrarCitas", "/nuevaCita").hasAnyRole("PATIENT"); //Páginas permitidas para Paciente
+                .antMatchers("/areaSanitario", "/indexAuth", "/loginExito", "/mostrarPacientes").hasAnyRole("HEALTHPERSONNEL") //Páginas permitidas para Sanitario
+                .antMatchers("/preguntasSinContestar", "/preguntasSinContestar/**", "/contestarPregunta/**").hasAnyRole("ADMIN", "HEALTHPERSONNEL")
+                .antMatchers("/appointmentAlreadyExist/**", "/appointment", "/appointmentNotFound", "/citaAgregada", "/cualDoctor", "/areaPaciente",
+                        "/indexAuth", "/loginExito", "/mostrarCitas", "/nuevaCita").hasAnyRole("PATIENT") //Páginas permitidas para Paciente
 
-                   
-                http
-                	.formLogin()
-                    .loginPage("/login") //Ruta login
-                    .usernameParameter("username")
-                    .passwordParameter("password")
-                    .defaultSuccessUrl("/loginExitoso") //Página a la que nos lleva al hacer el login
-                    .failureUrl("/loginError")
-                    .permitAll()
-                    .and()
+                .antMatchers("/myProfile").hasAnyRole("ADMIN", "HEALTHPERSONNEL", "PATIENT");
+
+
+        http
+                .formLogin()
+                .loginPage("/login") //Ruta login
+                .usernameParameter("username")
+                .passwordParameter("password")
+                .defaultSuccessUrl("/loginExitoso") //Página a la que nos lleva al hacer el login
+                .failureUrl("/loginError")
+                .permitAll()
+                .and()
                 .logout()
-                    .logoutUrl("/logout")//Url para deslogearse
-                    .logoutSuccessUrl("/performLogout") //Url de la zona pública
-                    .invalidateHttpSession(true)
-                    .deleteCookies("JSESSIONID")
-                    .and()
+                .logoutUrl("/logout")//Url para deslogearse
+                .logoutSuccessUrl("/performLogout") //Url de la zona pública
+                .invalidateHttpSession(true)
+                .deleteCookies("JSESSIONID")
+                .and()
                 .headers().frameOptions().disable();//Para poder acceder a la consola de h2
 
-            http
-                    .headers()
-                    .xssProtection();
+        http
+                .headers()
+                .xssProtection();
 
-            http.httpBasic();
-            
+        http.httpBasic();
+
     }
 }
